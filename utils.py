35e89420539113d6c82bcfdfc478d203d877eba2
Jason Ramapuram <jason.ramapuram@gmail.com>
2018-04-23 04:23:30 +0200

    add increment merger and helper

--- utils.py
+++ utils.py
@@ -53,18 +53,90 @@ def binarize(img, block_size=21):
                                  cv2.THRESH_BINARY, block_size, 0)
     return np.expand_dims(img, -1) if len(img.shape) < 3 else img
 
+
+def find_max_label(loader):
+    max_label = 0
+    for data, lbls in loader:
+        max_seen_lbl = max(lbls)
+        if max_seen_lbl > max_label:
+            max_label = max_seen_lbl
+
+    return max_label
+
+
+def label_offset_merger(loaders, batch_size, use_cuda=False):
+    ''' iterate over all the loaders and:
+           1. finds the max labels
+           2. increments loader2 with +loader1_max_label
+           3. build a new loader with all the data [uses simple_merger]'''
+    # step 1
+    max_labels_train = [find_max_label(loader.train_loader) for loader in loaders]
+    max_labels_test = [find_max_label(loader.test_loader) for loader in loaders]
+    max_labels = np.maximum(max_labels_test, max_labels_train) + 1
+    for j in range(1, len(max_labels)):
+        max_labels[j] += max_labels[j - 1]
+
+    print('determined offset max_labels: ', max_labels)
+    max_labels = torch.from_numpy(max_labels.astype(np.int32))
+
+    # step 2
+    def _extract_and_increment(loader, idx):
+            data_container, lbl_container = [], []
+            for data, labels in loader: # extract all the data
+                data_container.append(data)
+                lbl_container.append(labels)
+
+            # handle data concat
+            if isinstance(data_container[0], torch.Tensor):
+                data_container = torch.cat(data_container, 0)
+            elif isinstance(data_container[0], np.array):
+                data_container = torch.from_numpy(np.vstack(data_container))
+            else:
+                raise Exception("unknown data type")
+
+            # handle label concat
+            if isinstance(lbl_container[0], torch.Tensor):
+                lbl_container = torch.cat(lbl_container, 0)
+            elif isinstance(lbl_container[0], np.array):
+                lbl_container = torch.from_numpy(np.vstack(lbl_container))
+            else:
+                raise Exception("unknown label type")
+
+            # do the actual incrementing
+            lbl_container += max_labels[idx - 1]
+            dataset = torch.utils.data.TensorDataset(data_container, lbl_container)
+            kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+            return torch.utils.data.DataLoader(
+                dataset=dataset,
+                batch_size=loader.batch_size,
+                drop_last=True,
+                shuffle=True,
+                **kwargs
+            )
+
+    # recreate the fucking loaders
+    for i in range(1, len(max_labels)):
+        loaders[i].train_loader = _extract_and_increment(loaders[i].train_loader, i)
+        loaders[i].test_loader = _extract_and_increment(loaders[i].test_loader, i)
+        loaders[i].output_size = max_labels[i].cpu().item()
+
+    # step3: finally merge them with simpleMerger
+    return simple_merger(loaders, batch_size, use_cuda)
+
+
 def simple_merger(loaders, batch_size, use_cuda=False):
-    print("\nWARN [simplemerger]: no process in place for handling different classes\n")
+    print("""\nWARN [simplemerger]: no process in place for handling different classes,
+ignore this if you called label_offset_merger\n""")
     train_dataset = loaders[0].train_loader.dataset
     test_dataset = loaders[0].test_loader.dataset
     img_shp = loaders[0].img_shp
-    output_size = loaders[0].output_size
+    output_size = loaders[-1].output_size
 
     for loader in loaders[1:]:
         train_dataset += loader.train_loader.dataset
         test_dataset += loader.test_loader.dataset
         assert img_shp == loader.img_shp
-        assert output_size == loader.output_size
+        #assert output_size == loader.output_size
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     train_loader = torch.utils.data.DataLoader(
