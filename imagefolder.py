8164bdaf3f86dbf386ed54ad7045474f61be30ca
Jason Ramapuram <jason.ramapuram@gmail.com>
2020-06-19 16:18:48 +0200

    enable returning a non-augmented image in addition to the augmented ones

--- imagefolder.py
+++ imagefolder.py
@@ -1,6 +1,6 @@
 import os
 import functools
-from torchvision import datasets
+from torchvision import datasets, transforms
 from torchvision.datasets.folder import default_loader
 
 from .abstract_dataset import AbstractLoader
@@ -51,20 +51,40 @@ class ImageFolderLoader(AbstractLoader):
 class MultiAugmentImageDataset(datasets.ImageFolder):
     """Extends imagefolder to simply augment the same image num_augments times."""
 
-    def __init__(self, root, transform=None, target_transform=None,
+    def __init__(self, root, transform=None, target_transform=None, non_augmented_transform=None,
                  loader=default_loader, is_valid_file=None, num_augments=2):
         assert num_augments > 1, "Use this dataset when you want >1 augmentations"
         self.num_augments = num_augments  # Number of times to augment the same image
+        self.non_augment_transform = non_augmented_transform  # transform for non-augmented image (eg: resize)
 
         super(MultiAugmentImageDataset, self).__init__(
             root=root, transform=transform, target_transform=target_transform,
             loader=default_loader, is_valid_file=is_valid_file)
 
+    def __getitem_non_transformed__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        """
+        path, target = self.samples[index]
+        sample = self.loader(path)
+
+        if self.non_augment_transform is not None:
+            sample = self.non_augment_transform(sample)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return sample, target
+
     def __getitem__(self, index):
         """Label is the same for index, so just run augmentations again."""
-        sample0, target = super(MultiAugmentImageDataset, self).__getitem__(index)
+        sample0, target = self.__getitem_non_transformed__(index)
         samples = [sample0] + [super(MultiAugmentImageDataset, self).__getitem__(index)[0]
-                               for _ in range(self.num_augments - 1)]
+                               for _ in range(self.num_augments)]
         return samples + [target]
 
 
@@ -76,13 +96,16 @@ class MultiAugmentImageFolder(AbstractLoader):
                  train_transform=None, train_target_transform=None,
                  test_transform=None, test_target_transform=None,
                  valid_transform=None, valid_target_transform=None,
+                 non_augmented_transform=None,  # The first image returned is non-augmented, useful for resize, etc.
                  cuda=True, num_augments=2, **kwargs):
         # Curry the train and test dataset generators.
         train_generator = functools.partial(MultiAugmentImageDataset,
                                             root=os.path.join(path, 'train'),
+                                            non_augmented_transform=self.compose_transforms(non_augmented_transform),
                                             num_augments=num_augments)
         test_generator = functools.partial(MultiAugmentImageDataset,
                                            root=os.path.join(path, 'test'),
+                                           non_augmented_transform=self.compose_transforms(non_augmented_transform),
                                            num_augments=num_augments)
         valid_generator = None
         if os.path.isdir(os.path.join(path, 'valid')):
